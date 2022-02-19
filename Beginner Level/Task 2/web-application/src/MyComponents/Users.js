3940e288ab4d8ec619fcfb2e1796c0e6de558d41
nikhil0502 <naharnikhil6@gmail.com>
2022-02-19 17:43:01 +0530

    task 2

--- Beginner Level/Task 2/web-application/src/MyComponents/Users.js
+++ Beginner Level/Task 2/web-application/src/MyComponents/Users.js
@@ -1,36 +1,67 @@
 import React, { useState } from "react";
+import "../App.css";
+import axios from "axios";
+import { PushSpinner } from "react-spinners-kit";
 
 export const Users = () => {
-  const [users, setUsers] = useState([]);
+  const [list, setList] = useState([]);
+  const [loader, setLoader] = useState(true);
 
-  const loadApi = async () => {
-    const getData = await fetch("https://reqres.in/api/users?page=1");
-    const getResponse = await getData.json();
-    setUsers(getResponse.data);
+  const loadAPI = async (e) => {
+    setLoader(false);
+    e.preventDefault();
+    const response = await axios.get("https://reqres.in/api/users?page=1");
+    setList(response.data.data);
+    setTimeout(() => {
+      setLoader(true);
+    }, 2000);
   };
+
   return (
     <>
-      <div className="container my-5">
-        <button
-          className="btn btn-outline-success "
-          type="submit"
-          onClick={loadApi}
-        >
-          Get Users
-        </button>
-
-        <div className="container my-4">
-          <h5>Users Data:</h5>
-          <ul>
-            {users.map(({ id, email, first_name, last_name, avatar }) => (
-              <li key={id}>
-                Email: {email} <br /> First Name: {first_name} <br />
-                Last Name: {last_name}
-                <br /> Avatar: {avatar}
-                <br /> <hr />
-              </li>
-            ))}
-          </ul>
+      <div className="content">
+        <div className="form">
+          <div className="text">
+            {list.length === 0 ? (
+              <h5 className="my-3 py-3">
+                Click On the below button to get the data &#128521;
+              </h5>
+            ) : (
+              <div className="contents">
+                {loader ? (
+                  list.map((e) => (
+                    <p className="segment" key={e.id}>
+                      <div className="avatar">
+                        <img src={e.avatar} alt="" srcset="" />
+                      </div>
+                      <div className="name">
+                        <span>
+                          {e.first_name} {e.last_name}
+                        </span>
+                        <br />
+                        <span>{e.email}</span>
+                      </div>
+                    </p>
+                  ))
+                ) : (
+                  <div className="spinner">
+                    <PushSpinner
+                      className="spinner"
+                      size={50}
+                      color="#87CEEB"
+                    />
+                  </div>
+                )}
+              </div>
+            )}
+          </div>
+          <div>
+            <form onSubmit={loadAPI}>
+              <button type="submit" className="btn btn-outline-success">
+                Get Users
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </>
